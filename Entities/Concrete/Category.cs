d22e2b6cfbc8e41317d73ebb5377adeafb6ae657
FatihCengiz <fthcngz17@gmail.com>
2021-06-30 01:06:33 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Category:IEntity //işaretleme yapıyoruz
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
